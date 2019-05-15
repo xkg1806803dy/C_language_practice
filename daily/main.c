9c0f2128365383b42d0b72250f83d2b226464f5d
xkg1806803dy <48253251+xkg1806803dy@users.noreply.github.com>
2019-05-15 23:50:31 +0800

    Add files via upload

--- daily/main.c
+++ daily/main.c
@@ -1,64 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max  100
-struct nb
+struct smz
 {
-    int data;
-    struct nb *lchild,*rchild;
-}nb;
+    int cuoh2,fengziyu;
+    struct smz *lch,*rch;
+};
 
-void preorder(struct nb *root)
+int ap1,ap2;
+struct smz * srwde (struct smz *s,int h1,int h2)
 {
-    struct nb* s[max],*bt=root;
-    int top=-1;
-    while(bt!=NULL||top!=-1)
+    s=(struct smz *)malloc(sizeof(struct smz));
+    s->cuoh2=h2;
+    s->fengziyu=h1;
+    if(s->fengziyu!=0||s->cuoh2!=0)
     {
-        while(bt!=NULL)
+        if(s->fengziyu!=0)
+            s->fengziyu=h1-ap2;
+        else
+            s->cuoh2=h2-ap1;
+        s->lch=srwde(s->lch,s->fengziyu,s->cuoh2);
+        s->rch=srwde(s->rch,s->fengziyu,s->cuoh2);
+    }
+    return s;
+}
+float num=0,winf=0;
+void per(struct smz *s1)
+{
+    if(s1->cuoh2!=0||s1->fengziyu!=0)
+    {
+        per(s1->lch);
+        per(s1->rch);
+    }
+    else
+    {
+        if(s1->cuoh2==0)
+            num++;
+        else
         {
-            printf("%d",bt->data);
-            s[++top]=bt;
-            bt=bt->lchild;
-        }
-        if(top!=-1)
-        {
-            bt=s[top--];
-            bt=bt->rchild;
+            num++;
+            winf++;
         }
     }
 }
 
-struct dd
-{
-    struct nb *ptr;
-    int flag;
-}dd;
-void postorder(struct nb *root)
-{
-   struct  dd s1[max];
-   int top=-1;
-   struct nb *bt =root;
-    while(bt!=NULL||top!=-1)
-    {
-        while(bt!=NULL)
-        {
-            top++;
-            s1[top].ptr=bt;  s1[top].flag=1;
-            bt=bt->lchild;
-        }
-        while(top!=-1&&s1[top].flag==2)
-        {
-            bt=s1[top--].ptr;
-            printf("%d",bt->data);
-        }
-        if(top!=-1)
-        {
-            s1[top].flag=2;
-            bt=s1[top].ptr;
-        }
-    }
-}
 int main()
 {
-    printf("Hello world!\n");
+    int hp1,hp2;
+    scanf("%d%d%d%d",&hp1,&hp2,&ap1,&ap2);
+    struct smz hp;
+    hp.fengziyu=hp1;
+    hp.cuoh2=hp2;
+    hp=*srwde(&hp,hp1,hp2);
+    per(&hp);
+    printf("%.4f",winf/num*100);
     return 0;
 }
