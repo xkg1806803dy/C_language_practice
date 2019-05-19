9bb75a4ccf98a88262a737652ca78b0ace698717
xkg1806803dy <48253251+xkg1806803dy@users.noreply.github.com>
2019-05-19 23:39:51 +0800

    Add files via upload

--- daily/main.c
+++ daily/main.c
@@ -1,57 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct smz
-{
-    int cuoh2,fengziyu;
-    struct smz *lch,*rch;
-};
-
-int ap1,ap2;
-struct smz * srwde (struct smz *s,int h1,int h2)
-{
-    s=(struct smz *)malloc(sizeof(struct smz));
-    s->cuoh2=h2;
-    s->fengziyu=h1;
-    if(s->fengziyu!=0||s->cuoh2!=0)
-    {
-        if(s->fengziyu!=0)
-            s->fengziyu=h1-ap2;
-        else
-            s->cuoh2=h2-ap1;
-        s->lch=srwde(s->lch,s->fengziyu,s->cuoh2);
-        s->rch=srwde(s->rch,s->fengziyu,s->cuoh2);
-    }
-    return s;
-}
-float num=0,winf=0;
-void per(struct smz *s1)
-{
-    if(s1->cuoh2!=0||s1->fengziyu!=0)
-    {
-        per(s1->lch);
-        per(s1->rch);
-    }
-    else
-    {
-        if(s1->cuoh2==0)
-            num++;
-        else
-        {
-            num++;
-            winf++;
-        }
-    }
-}
-
+#include<math.h>
 int main()
 {
-    int hp1,hp2;
-    scanf("%d%d%d%d",&hp1,&hp2,&ap1,&ap2);
-    struct smz hp;
-    hp.fengziyu=hp1;
-    hp.cuoh2=hp2;
-    hp=*srwde(&hp,hp1,hp2);
-    per(&hp);
-    printf("%.4f",winf/num*100);
+    int A,B,n;
+    while(scanf("%d%d%d",&A,&B,&n)&&(A||B||n))
+    {
+        int i;
+        long int  x=1,y=1,s;
+        for(i=3;i<=n;i++)
+        {
+            s=(A*y+B*x)%7;
+            x=y; y=s;
+        }
+        printf("%ld\n",s);
+    }
     return 0;
 }
