04c41a14c76936711ef30559fe04cd7f114ae574
xkg1806803dy <48253251+xkg1806803dy@users.noreply.github.com>
2019-05-20 23:28:13 +0800

    Add files via upload

--- daily/main.c
+++ daily/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<math.h>
+
 int main()
 {
     int A,B,n;
     while(scanf("%d%d%d",&A,&B,&n)&&(A||B||n))
     {
-        int i;
-        long int  x=1,y=1,s;
-        for(i=3;i<=n;i++)
-        {
-            s=(A*y+B*x)%7;
-            x=y; y=s;
-        }
-        printf("%ld\n",s);
+        int i,a[50],s;
+        a[2]=1; a[1]=1;
+        for(i=3;i<=49;i++)
+            a[i]=(A*a[i-1]+B*a[i-2])%7;
+        s=n%49;
+        a[0]=a[49];
+        printf("%d\n",a[s]);
     }
     return 0;
 }
