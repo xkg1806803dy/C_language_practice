f6aa31e1df160e0dd32dd88d5970c91838808f32
xkg1806803dy <48253251+xkg1806803dy@users.noreply.github.com>
2019-06-10 23:39:01 +0800

    Add files via upload

--- daily/main.c
+++ daily/main.c
@@ -1,75 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*定义一个二维数组：
+#include<string.h>
+char sum[100010];
 
-int maze[5][5] = {
 
-0, 1, 0, 0, 0,
-
-0, 1, 0, 1, 0,
-
-0, 0, 0, 0, 0,
-
-0, 1, 1, 1, 0,
-
-0, 0, 0, 1, 0,
-
-};
-
-它表示一个迷宫，其中的1表示墙壁，0表示可以走的路，只能横着走或竖着走，不能斜着走，要求编程序找出从左上角到右下角的最短路线。*/
-int maze[5][5];
-int flag[5][5]= {0};
-
-int choose(int j,int i)
+struct sdd
 {
-    int sum=1;
-    while(j<5&&i<4&&j>=0&&i>=0)
-    {
-        if(maze[j][i+1]!=0&&flag[j][i+1]!=1)
-        {
-            flag[j][i+1]=1;
-            if( choose(j,i+1)==1)
-                printf("(%d,%d)\n",j,i+1);
-            else continue;
-        }
-        else  if(maze[j+1][i]!=0&&flag[j+1][i]!=1)
-        {
-            flag[j+1][i]=1;
-            if( choose(j+1,i)==1)
-                printf("(%d,%d)\n",j+1,i);
-            else continue;
-        }
-        else  if(maze[j][i-1]!=0&&flag[j][i-1]!=1)
-        {
-            flag[j][i-1]=1;
-            if( choose(j,i-1)==1)
-                printf("(%d,%d)\n",j,i-1);
-            else continue;
-        }
-        else  if(maze[j-1][i]!=0&&flag[j-1][i]!=1)
-        {
-            flag[j-1][i]=1;
-            if( choose(j-1,i)==1)
-                printf("(%d,%d)\n",j-1,i);
-            else continue;
-        }
-        else
-        {
-            sum=0;
-            break;
-        }
-    }
-    return sum;
-}
+    int num;
+    char s[10010];
+}aid[10010];
 
+void handle()
+{
+    int i,j=0,t=0;
+    struct sdd *p,*p1;
+    struct sdd aid[10010];
+    p1=p=&aid[0];
+    int len;
+    len=strlen(sum);
+    p=(struct sdd*)malloc(sizeof(struct sdd));
+    p->num=1;
+    for(i=0;i<len;i++)
+    {
+        if(sum[i]=='P') { p=&aid[++j];p->num=j+1;  p=(struct sdd*)malloc(sizeof(struct sdd)); t=strlen(p1); p1=p; continue; }
+        if(sum[i]=='B') { p=&aid[++j];p->num=j+1;  p=(struct sdd*)malloc(sizeof(struct sdd)); int x;
+        x=strlen(p1); strncpy(p,p1,x-1);t=x-1;  continue; }
+        p[j].s[t]=sum[i];
+    }
+}
 int main()
 {
-    int i,j;
-    for(i=0; i<5; i++)
-        for(j=0; j<5; j++)
-            scanf("%d",&maze[i][j]);
-    i=j=0;
-    printf("(%d,%d)\n",j,i);
-    choose(j,i);
+    int k,n,m,j,t;
+    scanf("%s",&sum);
+    handle();
+    scanf("%d",&k);
+    char ch[10010];
+    while(k--)
+    {
+        int tim=0,i;
+        scanf("%d%d",&n,&m);
+        t=strlen(aid[m-1].s)-strlen(aid[n-1].s);
+        strcpy(ch,aid[m-1].s);
+        if(t<0) {printf("0\n"); continue;}
+        for(i=0;i<t;i++)
+        {
+            if(strncmp(aid[n-1].s,ch,strlen(aid[n-1].s)==0)) tim++;
+            else
+            {
+                char a;
+                for(j=1;j<=strlen(ch);j++)
+                {
+                    a=ch[j];
+                    ch[j-1]=a;
+                }
+            }
+        }
+        printf("%d\n",tim);
+    }
     return 0;
 }
